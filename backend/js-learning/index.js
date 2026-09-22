d024b35f00aa4c36be641864c82d64056109c306
Oussama El Hassouni <hassouniff@gmail.com>
2026-09-22 20:14:18 +0000

    Learn arrow functions

--- backend/js-learning/index.js
+++ backend/js-learning/index.js
@@ -1,9 +1,9 @@
-// const add1 = (a, b) => {
-//     return a + b;
-// };
+// // const add1 = (a, b) => {
+// //     return a + b;
+// // };
 
-// const result1 = add1(10, 5);
-// console.log("10 + 5 =", result1);
+// // const result1 = add1(10, 5);
+// // console.log("10 + 5 =", result1);
 
 
 
@@ -11,17 +11,36 @@
 //     return a + b;
 // }
 
-// const result2 = add2(10, 5);
-// console.log("10 + 5 =", result2);
+
+// const r = add2(6, 1);
+// console.log(r);
+
+// // const result2 = add2(10, 5);
+// // console.log("10 + 5 =", result2);
 
 
 
-const user = {
-    name: "Oussama",
+// // const user = {
+// //     name: "Oussama",
 
-    sayName: () => {
-        console.log(this.name);
-    }
-};
+// //     sayName: () => {
+// //         console.log(this.name);
+// //     }
+// // };
 
-user.sayName(); // usually undefined
+// // user.sayName(); // usually undefined
+
+
+// const users = [
+//   { id: 12, name: "Alice" },
+//   { id: 73, name: "Bob" },
+//   { id: 99, name: "Charlie" }
+// ];
+
+// const user = users.find((user) => user.id === 73);
+// console.log(user);
+
+
+const ages = [3, 10, 18, 22];
+
+const age = ages.find((test)=> test < 13);
