4db2354264a152f31fa971a5e04705238b584cb8
Oussama El Hassouni <hassouniff@gmail.com>
2026-09-22 21:29:31 +0000

    Learn spread syntax

--- backend/js-learning/index.js
+++ backend/js-learning/index.js
@@ -41,6 +41,33 @@
 // console.log(user);
 
 
-const ages = [3, 10, 18, 22];
+// const ages = [3, 10, 18, 22, 15, 12, 5, 6];
 
-const age = ages.find((test)=> test < 13);
+// const age = ages.find((test)=> test < 13);
+
+
+const users = [
+	{ id: 1, name: "A", username: "a1", status: "active" },
+	{ id: 2, name: "B", username: "b2", status: "inactive" },
+	{ id: 3, name: "C", username: "c3", status: "pending" },
+	{ id: 4, name: "D", username: "d4", status: "active" }
+];
+
+// console.log(users.filter((user) => user.status === "active").map((u) => u.name));
+
+
+// console.log(users);
+
+
+const { username: hh , name} = users[0];
+// console.log(hh); // Output: "alice123"
+// console.log(name); // Output: "Alice"
+
+
+const usersCopy = [...users];
+const usersWithNewUser = [...users, { id: 5, name: "E", username: "e5", status: "inactive" }];
+const updatedUser = { ...users[0], status: "inactive" };
+
+// console.log(usersCopy);
+// console.log(usersWithNewUser);
+console.log(updatedUser);
